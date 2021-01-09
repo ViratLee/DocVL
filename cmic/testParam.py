9ed375365c237110ca64a42e26a8a237e56792b9
ViratLee <neungsay@gmail.com>
2021-01-09 15:24:50 +0700

    delete all files.

--- cmic/testParam.py
+++
@@ -1,7 +0,0 @@
-def method_01(param1, param3 = 0 , param2 = "not bark" ):
-	print('{} have {}, {}'.format(param1,param3, param2))
-def main():
-	method_01("dog", 4)
-	method_01("cat", param2='meaw meaw')
-if __name__ == '__main__':
-    main() 
